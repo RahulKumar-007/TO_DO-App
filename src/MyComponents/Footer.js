e5cc57ca2acb3d73bcb5f1d82bf6fee57c8e988f
Rahul <117337265+RahulKumar-007@users.noreply.github.com>
2023-08-21 02:05:57 +0530

    Update Footer.js

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
     return (
         <footer className="bg-dark text-light py-3">
             <p className="text-center">
-            Copyright &copy; MyTodosList.com
+            Copyright &copy; https://github.com/RahulKumar-007
             </p>
         </footer>
     )
